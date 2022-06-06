3a31d5ad0c86e5a5e092a960815b56dffc2f3ab7
Dary <dady281100@gmail.com>
2022-06-06 01:16:20 -0500

    Files uploaded

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=313812bb`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=313812bb`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
             displayMovieDetails(movieDetails);
